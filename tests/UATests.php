65d4d555cf38b992166af220bf29f1d571e70977
Mark Beech <mbeech@mark-beech.co.uk>
2016-07-20 21:32:00 +0100

    Make tests more readable

--- tests/UATests.php
+++ tests/UATests.php
@@ -21,7 +21,8 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         $this->CrawlerDetect = new CrawlerDetect();
     }
 
-    public function testBots()
+    /** @test */
+    public function user_agents_are_bots()
     {
         $lines = file(__DIR__.'/crawlers.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
@@ -31,7 +32,8 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         }
     }
 
-    public function testDevices()
+    /** @test */
+    public function user_agents_are_devices()
     {
         $lines = file(__DIR__.'/devices.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
@@ -41,7 +43,8 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         }
     }
 
-    public function testReturnsCorrectMatchedBotName()
+    /** @test */
+    public function it_returns_correct_matched_bot_name()
     {
         $test = $this->CrawlerDetect->isCrawler('Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) AppleWebKit (KHTML, like Gecko) Mobile (compatible; Yahoo Ad monitoring; https://help.yahoo.com/kb/yahoo-ad-monitoring-SLN24857.html)');
 
@@ -50,7 +53,8 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($this->CrawlerDetect->getMatches(), 'Yahoo Ad monitoring', $matches);
     }
 
-    public function testReturnsNullWhenNoBotDetected()
+    /** @test */
+    public function it_returns_null_when_no_bot_detected()
     {
         $test = $this->CrawlerDetect->isCrawler('nothing to see here');
 
@@ -59,14 +63,16 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($this->CrawlerDetect->getMatches(), null, $matches);
     }
 
-    public function testEmptyUserAgent()
+    /** @test */
+    public function empty_user_agent()
     {
         $test = $this->CrawlerDetect->isCrawler('      ');
 
         $this->assertEquals($test, false);
     }
 
-    public function testCurrentVisitor()
+    /** @test */
+    public function current_visitor()
     {
         $headers = (array) json_decode('{"DOCUMENT_ROOT":"\/home\/test\/public_html","GATEWAY_INTERFACE":"CGI\/1.1","HTTP_ACCEPT":"*\/*","HTTP_ACCEPT_ENCODING":"gzip, deflate","HTTP_CACHE_CONTROL":"no-cache","HTTP_CONNECTION":"Keep-Alive","HTTP_FROM":"bingbot(at)microsoft.com","HTTP_HOST":"www.test.com","HTTP_PRAGMA":"no-cache","HTTP_USER_AGENT":"Mozilla\/5.0 (compatible; bingbot\/2.0; +http:\/\/www.bing.com\/bingbot.htm)","PATH":"\/bin:\/usr\/bin","QUERY_STRING":"order=closingDate","REDIRECT_STATUS":"200","REMOTE_ADDR":"127.0.0.1","REMOTE_PORT":"3360","REQUEST_METHOD":"GET","REQUEST_URI":"\/?test=testing","SCRIPT_FILENAME":"\/home\/test\/public_html\/index.php","SCRIPT_NAME":"\/index.php","SERVER_ADDR":"127.0.0.1","SERVER_ADMIN":"webmaster@test.com","SERVER_NAME":"www.test.com","SERVER_PORT":"80","SERVER_PROTOCOL":"HTTP\/1.1","SERVER_SIGNATURE":"","SERVER_SOFTWARE":"Apache","UNIQUE_ID":"Vx6MENRxerBUSDEQgFLAAAAAS","PHP_SELF":"\/index.php","REQUEST_TIME_FLOAT":1461619728.0705,"REQUEST_TIME":1461619728}');
 
@@ -75,14 +81,16 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($cd->isCrawler(), true);
     }
 
-    public function testUserAgentPassedViaConstructor()
+    /** @test */
+    public function user_agent_passed_via_contructor()
     {
         $cd = new CrawlerDetect(null, 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) AppleWebKit (KHTML, like Gecko) Mobile (compatible; Yahoo Ad monitoring; https://help.yahoo.com/kb/yahoo-ad-monitoring-SLN24857.html)');
 
         $this->assertEquals($cd->isCrawler(), true);
     }
 
-    public function testHttpFromHeader()
+    /** @test */
+    public function http_from_header()
     {
         $headers = (array) json_decode('{"DOCUMENT_ROOT":"\/home\/test\/public_html","GATEWAY_INTERFACE":"CGI\/1.1","HTTP_ACCEPT":"*\/*","HTTP_ACCEPT_ENCODING":"gzip, deflate","HTTP_CACHE_CONTROL":"no-cache","HTTP_CONNECTION":"Keep-Alive","HTTP_FROM":"googlebot(at)googlebot.com","HTTP_HOST":"www.test.com","HTTP_PRAGMA":"no-cache","HTTP_USER_AGENT":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/28.0.1500.71 Safari\/537.36","PATH":"\/bin:\/usr\/bin","QUERY_STRING":"order=closingDate","REDIRECT_STATUS":"200","REMOTE_ADDR":"127.0.0.1","REMOTE_PORT":"3360","REQUEST_METHOD":"GET","REQUEST_URI":"\/?test=testing","SCRIPT_FILENAME":"\/home\/test\/public_html\/index.php","SCRIPT_NAME":"\/index.php","SERVER_ADDR":"127.0.0.1","SERVER_ADMIN":"webmaster@test.com","SERVER_NAME":"www.test.com","SERVER_PORT":"80","SERVER_PROTOCOL":"HTTP\/1.1","SERVER_SIGNATURE":"","SERVER_SOFTWARE":"Apache","UNIQUE_ID":"Vx6MENRxerBUSDEQgFLAAAAAS","PHP_SELF":"\/index.php","REQUEST_TIME_FLOAT":1461619728.0705,"REQUEST_TIME":1461619728}');
 
@@ -91,7 +99,8 @@ class UserAgentTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($cd->isCrawler(), true);
     }
 
-    public function testForRegexCollision()
+    /** @test */
+    public function there_are_no_regex_collisions()
     {
         $crawlers = new Crawlers();
 
