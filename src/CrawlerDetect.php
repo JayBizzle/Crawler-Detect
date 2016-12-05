496c957894228cef840e517a064ed0bac4486399
Mark Beech <mbeech@mark-beech.co.uk>
2016-12-05 20:51:49 +0000

    Remove grouped use statement

--- src/CrawlerDetect.php
+++ src/CrawlerDetect.php
@@ -11,7 +11,9 @@
 
 namespace Jaybizzle\CrawlerDetect;
 
-use Jaybizzle\CrawlerDetect\Fixtures\{Crawlers, Exclusions, Headers};
+use Jaybizzle\CrawlerDetect\Fixtures\Crawlers;
+use Jaybizzle\CrawlerDetect\Fixtures\Exclusions;
+use Jaybizzle\CrawlerDetect\Fixtures\Headers;
 
 class CrawlerDetect
 {
