21dba329f4fbda473caf859a48b8fe94cd13775e
Mark Beech <mbeech@mark-beech.co.uk>
2017-04-24 22:36:25 +0100

    CS

--- src/Detectors/IpDetector.php
+++ src/Detectors/IpDetector.php
@@ -49,34 +49,33 @@ class IpDetector
                 // $netmask is a CIDR size block
                 // fix the range argument
                 $x = explode('.', $range);
+
                 while (count($x) < 4) {
                     $x[] = '0';
                 }
+
                 list($a, $b, $c, $d) = $x;
                 $range = sprintf('%u.%u.%u.%u', empty($a) ? '0' : $a, empty($b) ? '0' : $b, empty($c) ? '0' : $c, empty($d) ? '0' : $d);
                 $range_dec = ip2long($range);
                 $ip_dec = ip2long($ip);
 
-          # Strategy 1 - Create the netmask with 'netmask' 1s and then fill it to 32 with 0s
-          #$netmask_dec = bindec(str_pad('', $netmask, '1') . str_pad('', 32-$netmask, '0'));
-
-          # Strategy 2 - Use math to create it
-          $wildcard_dec = pow(2, (32 - $netmask)) - 1;
-                $netmask_dec = ~$wildcard_dec;
+				// Use math to create create the netmask with 'netmask' 1s and then fill it to 32 with 0s
+				$wildcard_dec = pow(2, (32 - $netmask)) - 1;
+				$netmask_dec = ~$wildcard_dec;
 
                 return (($ip_dec & $netmask_dec) == ($range_dec & $netmask_dec));
             }
         } else {
             // range might be 255.255.*.* or 1.2.3.0-1.2.3.255
-        if (strpos($range, '*') !== false) { // a.b.*.* format
-          // Just convert to A-B format by setting * to 0 for A and 255 for B
-          $lower = str_replace('*', '0', $range);
-            $upper = str_replace('*', '255', $range);
-            $range = "$lower-$upper";
-        }
+			if (strpos($range, '*') !== false) { // a.b.*.* format
+				// Just convert to A-B format by setting * to 0 for A and 255 for B
+				$lower = str_replace('*', '0', $range);
+				$upper = str_replace('*', '255', $range);
+				$range = "$lower-$upper";
+			}
 
             if (strpos($range, '-') !== false) { // A-B format
-          list($lower, $upper) = explode('-', $range, 2);
+				list($lower, $upper) = explode('-', $range, 2);
                 $lower_dec = (float) sprintf('%u', ip2long($lower));
                 $upper_dec = (float) sprintf('%u', ip2long($upper));
                 $ip_dec = (float) sprintf('%u', ip2long($ip));
@@ -84,9 +83,7 @@ class IpDetector
                 return (($ip_dec >= $lower_dec) && ($ip_dec <= $upper_dec));
             }
 
-            echo 'Range argument is not in 1.2.3.4/24 or 1.2.3.4/255.255.255.0 format';
-
-            return false;
+            throw new Exception('Range argument is not in 1.2.3.4/24 or 1.2.3.4/255.255.255.0 format');
         }
     }
 }
