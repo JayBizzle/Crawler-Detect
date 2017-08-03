630b23bbb922d422f6aecbaae673af8f7a2d5b12
Jacob D. Eisinger <jacob@jobs2careers.com>
2017-08-03 11:53:26 -0500

    add Jobg8 URL Monitor

--- src/Fixtures/Crawlers.php
+++ src/Fixtures/Crawlers.php
@@ -383,6 +383,7 @@ class Crawlers extends AbstractProvider
         'Jaunt\/',
         'Jigsaw',
         'Jobboerse',
+        'Jobg8 URL Monitor',
         'jobo',
         'Jobrapido',
         'JS-Kit',
