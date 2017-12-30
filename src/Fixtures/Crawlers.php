a1475ce7114e703c36006b68de6b2762b7f196b9
Punit Sethi <punit@tezify.com>
2017-12-30 10:49:06 +0530

    Added HubSpot Page Screenshot Service crawler regex

--- src/Fixtures/Crawlers.php
+++ src/Fixtures/Crawlers.php
@@ -849,5 +849,6 @@ class Crawlers extends AbstractProvider
         'ZnajdzFoto',
         'ZyBorg',
         '[a-z0-9\-_]*((?<!cu)bot|crawler|archiver|transcoder|spider|uptime|validator|fetcher)',
+        'HubSpot Page Screenshot Service',
     );
 }
