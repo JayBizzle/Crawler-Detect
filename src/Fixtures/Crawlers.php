0928e53b132245648129711d8ca5d4e09d5ff7e9
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2021-05-23 08:47:24 +0200

    Add Hardenize.com crawler detection

--- src/Fixtures/Crawlers.php
+++ src/Fixtures/Crawlers.php
@@ -495,6 +495,7 @@ class Crawlers extends AbstractProvider
         'hackney\/',
         'Hadi Agent',
         'HappyApps-WebCheck',
+        'Hardenize',
         'Hatena',
         'Havij',
         'HaxerMen',
