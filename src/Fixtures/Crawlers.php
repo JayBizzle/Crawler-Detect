128a2af1c613b3936a3f57afc36557b5cad8b325
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2021-05-24 08:59:56 +0200

    Add CISPA Web Analyser detection

--- src/Fixtures/Crawlers.php
+++ src/Fixtures/Crawlers.php
@@ -228,6 +228,7 @@ class Crawlers extends AbstractProvider
         'Chlooe',
         'Chromaxa',
         'CirrusExplorer',
+        'CISPA Web Analyser',
         'CISPA Vulnerability Notification',
         'Citoid',
         'CJNetworkQuality',
